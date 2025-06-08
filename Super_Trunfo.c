66f2df07a586dd350123ec3285ef344acda0982a
Kabral10 <luiztw.tv@gmail.com>
2025-06-08 11:57:14 +0000

    adicionando informações manualmente

---
+++ Super_Trunfo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+
+*/
+
+char codigo[4], estado[20], cidade[15] ;
+int pontos ; 
+float PIB,area,população ;
+
+int main(){
+    printf("\n Super Trunfo Países \n");
+    printf("Insira o codigo da carta: \n");
+    scanf("%s", &codigo);
+    printf("Insira o nome do estado: \n");
+    scanf("%s", &estado);
+    printf("Insira o nome da cidade: \n");
+    scanf("%s", &cidade);
+}
