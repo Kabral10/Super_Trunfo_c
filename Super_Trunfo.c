b8052b9ba57063296130739c5fcd3d6053f13ffa
Kabral10 <luiztw.tv@gmail.com>
2025-06-09 03:36:15 +0000

    a few changes

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 /*
-
+1º Definir as variaveis principais
+2º Definir os inserts
+3º Exibir as informações selecionadas
+4º Organizar
 */
 
-char codigo[4], estado[20], cidade[15];
-int pontos; 
-float PIB, area, populacao;
+char codigo1[4], codigo2[4], codigo3[4], codigo4[4], codigo5[4], codigo6[4], codigo7[4], codigo8[4],
+     cidade1, cidade2, cidade3, cidade4, cidade5, cidade6, cidade7, cidade8, esta1, esta2, esta3, esta4, esta5, esta6, esta7, esta8;
+float pib1, pib2, pib3, pib4, pib5, pib6, pib7, pib8, area1, area2, area3, area4, area5, area6, area7, area8, pop1, pop2, pop3, pop4,
+      pop5, pop6, pop7, pop8;
+int  tur1, tur2, tur3, tur4, tur5, tur6, tur7, tur8, opc;
 
 int main(){
-    printf("\n Super Trunfo Países \n");
-    printf("Insira o codigo da carta: \n");
-    scanf("%s", &codigo);
-    printf("Insira o nome do estado: \n");
-    scanf("%s", &estado);
-    printf("Insira o nome da cidade: \n");
-    scanf("%s", &cidade);
+//primeira carta
+printf("\n- Super trunfo paises -\n");
+while (opc!=3){
+printf("Selecione uma opção:\n");
+printf("1 -> Registrar carta:");
+printf("2 -> Comparar carta:");
+printf("3 -> Sair:");
+    if (opc = 1);{ 
+        printf("\n --Registro-- \n");
+        printf("Digite o codigo da carta: \n");
+        scanf("%s", &codigo1);
+         
+     }
+    
+return 0;
+}
 }
