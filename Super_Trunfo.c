58bed2efa2bd9971d5c2c301544fca6b48dbaac1
Kabral10 <luiztw.tv@gmail.com>
2025-06-11 22:13:02 +0000

    Mudança na inserção de informações para fgets

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void removernovalinha(char *str) {
+    str[strcspn(str, "\n")] = '\0';}
 /*
 Criar um programa em C que permita ao usuário inserir os dados de DUAS CARTAS do Super Trunfo. Para cada carta, o usuário deverá fornecer as seguintes informações:
 Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
@@ -11,7 +13,7 @@ PIB: O Produto Interno Bruto da cidade. Tipo: float
 Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int
 */
 
-char codigo1[4], codigo2[4], nome1[20], nome2[20], estado1[20], estado2[20];
+char codigo1[4], codigo2[4], nome1[30], nome2[30], estado1[30], estado2[30];
 int popula1, popula2, turismo1, turismo2;
 float PIB1, PIB2, area1, area2;
 int main(){
@@ -19,43 +21,58 @@ int main(){
     printf("\n-- Criando Cartas --\n");
 // primeira carta
     printf("\nPrimeira carta\n");
+
     printf("Escreva o codigo da primeira carta: \n");
     scanf("%s", &codigo1);
+
     printf("Escreva o nome do estado: \n");
-    scanf("%s", &estado1);
-    fgets(estado1,20,stdin);
+    fgets(estado1,30,stdin);
+    removernovalinha(estado1);
+
     printf("Escreva o nome da cidade: \n");
     fgets(nome1,20,stdin);
-    scanf("%s", &nome1);
+    removernovalinha(nome1);
+
     printf("Qual a população da cidade: \n");
     scanf("%d", &popula1);
+
     printf("Quantos pontos turisticos existem nessa cidade: \n");
     scanf("%d", &turismo1);
+
     printf("Escreva o produto interno bruto (PIB) da cidade: \n");
     scanf("%f", &PIB1);
+
     printf("Qual a area da cidade em km²: \n");
     scanf("%f", &area1);
 
 //segunda carta
     printf("\nSegunda carta\n");
+
     printf("Escreva o codigo da segunda carta: \n");
     scanf("%s", &codigo2);
+
     printf("Escreva o nome do estado: \n");
     fgets(estado2,20,stdin);
-    scanf(" %s", &estado2);
+    removernovalinha(estado2);
+
     printf("Escreva o nome da cidade: \n");
     fgets(nome2,20,stdin);
-    scanf(" %s", &nome2);
+    removernovalinha(nome2);
+
     printf("Qual a população da cidade: \n");
     scanf("%d", &popula2);
+
     printf("Quantos pontos turisticos existem nessa cidade: \n");
     scanf("%d", &turismo2);
+
     printf("Escreva o produto interno bruto (PIB) da cidade: \n");
     scanf("%f", &PIB2);
+
     printf("Qual a area da cidade em km²: \n");
     scanf("%f", &area2);
 
 //Exibindo informações
+
 //primeira carta
     printf("\n-- Exibição de cartas --\n");
     printf("\n--Primeira Carta--\n");
